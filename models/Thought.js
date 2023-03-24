9bc8c1188b5f55622e1dd5eedc23c7ccb9c72f5f
JoshuaHamann <joshwashku@yahoo.com>
2023-03-23 22:39:49 -0500

    added validators and a subdocument for thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,14 +1,43 @@
 const { Schema, model } = require('mongoose');
 
+// subdocument to thoughtSchema
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Schema.Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+    }
+});
+
 const thoughtSchema = new Schema({
     thoughtText: { 
         type: String,
-        required: true },
-    createdAt: { type: Date, default: () => Date.now()},
-    username: { type: String, required: true },
-    reactions: [
-        {}
-    ]
+        required: true,
+        minLength: 1,
+        maxLength: 128 
+    },
+    createdAt: { 
+        type: Date, 
+        default: () => Date.now(),
+
+    },
+    username: { 
+        type: String, 
+        required: true 
+    },
+    reactions: [reactionSchema]
 
 });
 
