3b9a439276bcf264a439c30bfefc31062d0258ee
JoshuaHamann <joshwashku@yahoo.com>
2023-03-24 23:41:29 -0500

    created get and post routes for thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,27 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    // Write functions for routes
+    // Get all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a thought by Id
+    getSingleThought(req,res) {
+        Thought.findOne({ _id: req.params.thoughtId})
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought matched!" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Create a thought
+    createThought(req,res) {
+        Thought.create(req.body)
+            .then((thoughtData) => res.json(thoughtData))
+            .catch((err) => res.status(500).json(err));
+    },
 };
